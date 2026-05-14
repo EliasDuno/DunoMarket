bbec5503f3e18456d432cce8beda18d99a9667c8
EliasDuno <eliasduno@gmail.com>
2026-05-14 17:58:59 -0400

    Refactor database connection to use config function

--- check_commitments.js
+++ check_commitments.js
@@ -1,13 +1,7 @@
 const { Pool } = require('pg');
-require('dotenv').config();
+const { getMasterPoolConfig } = require('./config/db');
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-});
+const pool = new Pool(getMasterPoolConfig());
 
 async function verifyTable() {
     try {
